87457a9935754e046474cd503e63091dfe7bc2eb
leonardo <leonardo.duccini@gmail.com>
2019-04-22 10:44:39 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+// Copyright (c) 2018 ml5
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+/* ===
+ml5 Example
+Image Classification using Feature Extraction with MobileNet. Built with p5.js
+This example uses a callback pattern to create the classifier
+=== */
+
+let featureExtractor;
+let classifier;
+let video;
+let loss;
+let dogImages = 0;
+let catImages = 0;
+
+function setup() {
+  noCanvas();
+  // Create a video element
+  video = createCapture(VIDEO);
+  
+  featureExtractor = ml5.featureExtractor('MobileNet', modelReady);
+  // Create a new classifier using those features and give the video we want to use
+  classifier = featureExtractor.classification(video, videoReady);
+  console.log(video);
+  // Set up the UI buttons
+  
+}
+
+// A function to be called when the model has been loaded
+function modelReady() {
+   
+  select('#modelStatus').html('Base Model (MobileNet) Loaded!');
+  classifier.load('https://github.com/leo70/chat/blob/master/model.json', function() {
+    console.log("Custom Model is ready");
+    classify();
+  });
+}
+
+// A function to be called when the video has loaded
+function videoReady () {
+  select('#videoStatus').html('Video ready!');
+}
+
+// Classify the current frame.
+function classify() {
+    
+  classifier.classify(gotResults);
+}
+
+
+// Show the results
+function gotResults(err, results) {
+  // Display any error
+  if (err) {
+    console.error(err);
+  }
+  if (results && results[0]) {
+    select('#result').html(results[0].label);
+    select('#confidence').html(results[0].confidence);
+    classify();
+  }
+}
